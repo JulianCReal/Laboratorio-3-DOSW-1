5d5cafbc5af2cd7a1013a84f76ec0acd4f201fdd
Karol2905 <Karol.estupinan-v@mail.escuelaing.edu.co>
2025-08-28 18:48:04 -0500

    Addicon de dependencias y estructura

---
+++ IdeaProjects/Laboratorio-3-DOSW-1/README.md
@@ -0,0 +1,5 @@
+**Parte Inicial**
+
+![alt text](<Captura de pantalla de 2025-08-28 18-44-38.png>)
+
+
--- IdeaProjects/Laboratorio-3-DOSW-1/src/main/java/edu/dosw/lab/Application.java
+++ IdeaProjects/Laboratorio-3-DOSW-1/src/main/java/edu/dosw/lab/Application.java
@@ -4,7 +4,7 @@ package edu.dosw.lab;
  * Hello world!
  *
  */
-public class App 
+public class Application
 {
     public static void main( String[] args )
     {
